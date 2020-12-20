86451d29e21be7b113988596c76c4ffbbf4554f9
lihongwei <lihongwei3@dankegongyu.com>
2020-12-20 12:52:09 +0800

    first commit

---
+++ src/NucarfRpc/Impl/Tag/Tag.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lihongwei
+ * Date: 2020-12-20
+ * Time: 12:40
+ */
+namespace Nucarf\NucarfRpc\Impl\Tag;
+
+class Tag
+{
+
+}
---
+++ src/NucarfRpc/Service/Service.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lihongwei
+ * Date: 2020-12-20
+ * Time: 12:27
+ */
+
+namespace Nucarf\NucarfRpc\Server;
+
+error_reporting(E_ALL);
+
+define('THRIFT_ROOT', __DIR__);
+echo __DIR__;exit;
+require_once  THRIFT_ROOT.'/Thrift/ClassLoader/ThriftClassLoader.php';
+require_once __DIR__ . '/vendor/autoload.php';
+
+use Thrift\ClassLoader\ThriftClassLoader;
+
+$loader = new ThriftClassLoader();
+$loader->registerNamespace('Thrift', THRIFT_ROOT);
+$loader->register();
+
+
+use Thrift\Exception\TException;
+use Thrift\Factory\TTransportFactory;
+use Thrift\Factory\TBinaryProtocolFactory;
+use Thrift\TMultiplexedProcessor;
+use Thrift\Server\TServerSocket;
+use Thrift\Server\TSimpleServer;
+use Thrift\Transport\TBufferedTransport;
+use Thrift\Transport\TPhpStream;
+use Thrift\Protocol\TBinaryProtocol;
+use Thrift\server\TSwooleServerTransport;
+use Thrift\server\TSwooleServer;
+use Thrift\Transport\TSocket;
+use Nucarf\NucarfRpc\Impl\Tag;
+use Goods\Rpc\Attr\HelloWorldProcessor;
+use Goods\Rpc\Tag\TagServiceProcessor;
+
+class Service
+{
+
+}
